cc71a823e752e89d85c96aa2da1535d511865abc
ngvn185 <ngvn185@noreply.localhost>
2026-06-30 07:52:48 +0530

    remove service configs

--- src/main/java/org/ngs/auth/repository/ServiceAuthConfigRepository.java
+++
@@ -1,9 +0,0 @@
-package org.ngs.auth.repository;
-
-import org.ngs.auth.entity.ServiceAuthConfigEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ServiceAuthConfigRepository extends JpaRepository<ServiceAuthConfigEntity, Long> {
-}
--- src/main/java/org/ngs/auth/repository/ServiceAuthConfigSignUpMethodRepository.java
+++
@@ -1,9 +0,0 @@
-package org.ngs.auth.repository;
-
-import org.ngs.auth.entity.ServiceAuthConfigSignUpMethodEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ServiceAuthConfigSignUpMethodRepository extends JpaRepository<ServiceAuthConfigSignUpMethodEntity, Long> {
-}
